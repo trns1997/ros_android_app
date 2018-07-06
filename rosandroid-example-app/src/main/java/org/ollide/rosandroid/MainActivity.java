058f5b280c7067437bd5553bc94d1db2109c8778
trns1997 <trns@ust.hk>
2018-07-06 13:50:59 +0530

    clean

--- rosandroid-example-app/src/main/java/org/ollide/rosandroid/MainActivity.java
+++ rosandroid-example-app/src/main/java/org/ollide/rosandroid/MainActivity.java
@@ -71,13 +71,13 @@ public class MainActivity extends RosActivity {
             return GraphName.of("move_base_simple/goal");
         }
 
-        private ArrayList<SampleModel> createSampleData(){
+        private ArrayList<SampleModel> createSampleData() {
             ArrayList<SampleModel> items = new ArrayList<>();
-            items.add(new SampleModel("First item"));
-            items.add(new SampleModel("Second item"));
-            items.add(new SampleModel("Third item"));
-            items.add(new SampleModel("The ultimate item"));
-            items.add(new SampleModel("Last item"));
+            items.add(new SampleModel("Start"));
+            items.add(new SampleModel("Pos1"));
+            items.add(new SampleModel("Pos2"));
+            items.add(new SampleModel("Pos3"));
+            items.add(new SampleModel("Pos4"));
             items.add(new SampleModel("Lorem ipsum"));
             items.add(new SampleModel("Dolor sit"));
             items.add(new SampleModel("Some random word"));
@@ -87,95 +87,105 @@ public class MainActivity extends RosActivity {
 
         @Override
         public void onStart(ConnectedNode connectedNode) {
-                final Publisher<geometry_msgs.PoseStamped> publisher =
-                        connectedNode.newPublisher("move_base_simple/goal", "geometry_msgs/PoseStamped");
+            final Publisher<geometry_msgs.PoseStamped> publisher =
+                    connectedNode.newPublisher("move_base_simple/goal", "geometry_msgs/PoseStamped");
 
-                Button button = findViewById(R.id.button);
-                button.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        SimpleSearchDialogCompat<SampleModel> dialog = new SimpleSearchDialogCompat<>(MainActivity.this, "Search...",
-                                "What are you looking for...?", null, createSampleData(),
-                                new SearchResultListener<SampleModel>() {
-                                    @Override
-                                    public void onSelected(BaseSearchDialogCompat dialog,
-                                                           SampleModel item, int position) {
-                                        Toast.makeText(MainActivity.this, item.getTitle(),
-                                                Toast.LENGTH_SHORT).show();
-                                        dialog.dismiss();
+            Button button = findViewById(R.id.button);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    SimpleSearchDialogCompat<SampleModel> dialog = new SimpleSearchDialogCompat<>(MainActivity.this, "Search...",
+                            "What are you looking for...?", null, createSampleData(),
+                            new SearchResultListener<SampleModel>() {
+                                @Override
+                                public void onSelected(BaseSearchDialogCompat dialog,
+                                                       SampleModel item, int position) {
+                                    Toast.makeText(MainActivity.this, item.getTitle(),
+                                            Toast.LENGTH_SHORT).show();
+                                    dialog.dismiss();
 
-                                        switch (item.getTitle()) {
-                                            case "First item":
-                                                Log.i("mssg", "First item");
-                                                PoseStamped pose = publisher.newMessage();
-                                                pose.getHeader().setFrameId("map");
-                                                pose.getHeader().setStamp(Time.fromMillis(System.currentTimeMillis()));
-                                                pose.getPose().getOrientation().setW(0.893834884065);
-                                                pose.getPose().getOrientation().setX(0);
-                                                pose.getPose().getOrientation().setY(0);
-                                                pose.getPose().getOrientation().setZ(0.448396253362);
-                                                pose.getPose().getPosition().setX(0.0512188374996);
-                                                pose.getPose().getPosition().setY(-0.120340190828);
-                                                pose.getPose().getPosition().setZ(0);
-                                                publisher.publish(pose);
-                                                break;
-                                            case "Second item":
-                                                Log.i("mssg", "Second item");
-                                                PoseStamped second = publisher.newMessage();
-                                                second.getHeader().setFrameId("map");
-                                                second.getHeader().setStamp(Time.fromMillis(System.currentTimeMillis()));
-                                                second.getPose().getOrientation().setW(0.79864677185);
-                                                second.getPose().getOrientation().setX(0);
-                                                second.getPose().getOrientation().setY(0);
-                                                second.getPose().getOrientation().setZ(0.601800077944);
-                                                second.getPose().getPosition().setX(0.235075980425);
-                                                second.getPose().getPosition().setY(-2.96770429611);
-                                                second.getPose().getPosition().setZ(0);
-                                                publisher.publish(second);
-                                                break;
-                                            case "Third item":
-                                                Log.i("mssg", "Third item");
-                                                PoseStamped third = publisher.newMessage();
-                                                third.getHeader().setFrameId("map");
-                                                third.getHeader().setStamp(Time.fromMillis(System.currentTimeMillis()));
-                                                third.getPose().getOrientation().setW(0.642132484179);
-                                                third.getPose().getOrientation().setX(0);
-                                                third.getPose().getOrientation().setY(0);
-                                                third.getPose().getOrientation().setZ(-0.76659368166);
-                                                third.getPose().getPosition().setX(0);
-                                                third.getPose().getPosition().setY(0);
-                                                third.getPose().getPosition().setZ(0);
-                                                publisher.publish(third);
-                                                break;
-                                        }
+                                    switch (item.getTitle()) {
+                                        case "Start":
+                                            Log.i("mssg", "Start");
+                                            PoseStamped pose = publisher.newMessage();
+                                            pose.getHeader().setFrameId("map");
+                                            pose.getHeader().setStamp(Time.fromMillis(System.currentTimeMillis()));
+                                            pose.getPose().getOrientation().setW(0.99999809223);
+                                            pose.getPose().getOrientation().setX(0);
+                                            pose.getPose().getOrientation().setY(0);
+                                            pose.getPose().getOrientation().setZ(0.00195333988248);
+                                            pose.getPose().getPosition().setX(0);
+                                            pose.getPose().getPosition().setY(0);
+                                            pose.getPose().getPosition().setZ(0);
+                                            publisher.publish(pose);
+                                            break;
+
+                                        case "Pos1":
+                                            Log.i("mssg", "Pos1");
+                                            PoseStamped second = publisher.newMessage();
+                                            second.getHeader().setFrameId("map");
+                                            second.getHeader().setStamp(Time.fromMillis(System.currentTimeMillis()));
+                                            second.getPose().getOrientation().setW(0.102068293041);
+                                            second.getPose().getOrientation().setX(0);
+                                            second.getPose().getOrientation().setY(0);
+                                            second.getPose().getOrientation().setZ(0.994777393971);
+                                            second.getPose().getPosition().setX(-1.03944051266);
+                                            second.getPose().getPosition().setY(2.54983639717);
+                                            second.getPose().getPosition().setZ(0);
+                                            publisher.publish(second);
+                                            break;
+
+                                        case "Pos2":
+                                            Log.i("mssg", "Pos2");
+                                            PoseStamped third = publisher.newMessage();
+                                            third.getHeader().setFrameId("map");
+                                            third.getHeader().setStamp(Time.fromMillis(System.currentTimeMillis()));
+                                            third.getPose().getOrientation().setW(0.935369518692);
+                                            third.getPose().getOrientation().setX(0);
+                                            third.getPose().getOrientation().setY(0);
+                                            third.getPose().getOrientation().setZ(-0.353671971609);
+                                            third.getPose().getPosition().setX(-8.87926959991);
+                                            third.getPose().getPosition().setY(3.08588671684);
+                                            third.getPose().getPosition().setZ(0);
+                                            publisher.publish(third);
+                                            break;
+
+                                        case "Pos3":
+                                            Log.i("mssg", "Pos3");
+                                            PoseStamped fourth = publisher.newMessage();
+                                            fourth.getHeader().setFrameId("map");
+                                            fourth.getHeader().setStamp(Time.fromMillis(System.currentTimeMillis()));
+                                            fourth.getPose().getOrientation().setW(-0.146961731995);
+                                            fourth.getPose().getOrientation().setX(0);
+                                            fourth.getPose().getOrientation().setY(0);
+                                            fourth.getPose().getOrientation().setZ(0.989142178521);
+                                            fourth.getPose().getPosition().setX(-1.84649252892);
+                                            fourth.getPose().getPosition().setY(-1.1724768877);
+                                            fourth.getPose().getPosition().setZ(0);
+                                            publisher.publish(fourth);
+                                            break;
+
+                                        case "Pos4":
+                                            Log.i("mssg", "Pos4");
+                                            PoseStamped fifth = publisher.newMessage();
+                                            fifth.getHeader().setFrameId("map");
+                                            fifth.getHeader().setStamp(Time.fromMillis(System.currentTimeMillis()));
+                                            fifth.getPose().getOrientation().setW(0.998230198425);
+                                            fifth.getPose().getOrientation().setX(0);
+                                            fifth.getPose().getOrientation().setY(0);
+                                            fifth.getPose().getOrientation().setZ(-0.05946823482);
+                                            fifth.getPose().getPosition().setX(-9.52041339874);
+                                            fifth.getPose().getPosition().setY(-0.241153270006);
+                                            fifth.getPose().getPosition().setZ(0);
+                                            publisher.publish(fifth);
+                                            break;
                                     }
-                                });
-                        dialog.show();
-                        dialog.getSearchBox().setTypeface(Typeface.SERIF);
-                    }
-                });
-
-//            final Button wall = findViewById(R.id.wall);
-//            final Button trash = findViewById(R.id.trash);
-//            final Button start = findViewById(R.id.start);
-//            trash.setOnClickListener(new Vi   ew.OnClickListener() {
-//                public void onClick(View v) {
-
-//                }
-//            });
-//            wall.setOnClickListener(new View.OnClickListener() {
-//                public void onClick(View v) {
-
-//                }
-//            });
-//            start.setOnClickListener(new View.OnClickListener() {
-//                public void onClick(View v) {
-
-//                }
-//            });
-
-
+                                }
+                            });
+                    dialog.show();
+                    dialog.getSearchBox().setTypeface(Typeface.SERIF);
+                }
+            });
         }
     }
-
 }
